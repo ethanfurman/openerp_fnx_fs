dc6c32023f6ea5489930f5aa70bf066f31bc8f03
Ethan Furman <ethan@stoneleaf.us>
2017-09-09 11:24:59 -0700

    add error checking around file access

--- controllers.py
+++ controllers.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import openerp
+import logging
 import werkzeug
 from openerp.addons.web.http import Controller, httprequest
 from openerp.addons.web.controllers.main import content_disposition
@@ -8,6 +8,7 @@ from antipathy import Path
 from fnx_fs.server import permissions
 from mimetypes import guess_type
 
+_logger = logging.getLogger(__name__)
 fnx_root = Path('/var/openerp/fnxfs/')
 
 class FnxFS(Controller):
@@ -22,27 +23,32 @@ class FnxFS(Controller):
     @httprequest
     def get_file(self, request):
         target_file = Path(request.httprequest.path[7:])
-        if not (fnx_root/target_file).exists():
-            return request.not_found('unable to find %s' % (fnx_root/target_file,))
+        target_path_file = fnx_root/target_file
+        if not target_path_file.exists() or target_path_file.isdir():
+            return request.not_found('%s is not available at this time.' % (target_file,))
         master_session_id = request.httprequest.cookies['instance0|session_id'].replace('%22','')
         if master_session_id:
-            master_session = request.httpsession[master_session_id]
-            login = master_session._login
-            perms = permissions[target_file]
-            access = perms.create_delete_users + perms.write_users + perms.read_users
-            if login in access or 'all' in access:
-                with (fnx_root/target_file).open('rb') as fh:
-                    file_data = fh.read()
-                return request.make_response(
-                        file_data,
-                        headers=[
-                            ('Content-Disposition',  content_disposition(target_file.filename, request)),
-                            ('Content-Type', guess_type(target_file.filename)[0] or 'octet-stream'),
-                            ('Content-Length', len(file_data)),
-                            ],
-                        )
+            try:
+                master_session = request.httpsession[master_session_id]
+                login = master_session._login
+                perms = permissions[target_file]
+                access = perms.write_users + perms.read_users
+                if login in access or 'all' in access:
+                    with (target_path_file).open('rb') as fh:
+                        file_data = fh.read()
+                    return request.make_response(
+                            file_data,
+                            headers=[
+                                ('Content-Disposition',  content_disposition(target_file.filename, request)),
+                                ('Content-Type', guess_type(target_file.filename)[0] or 'octet-stream'),
+                                ('Content-Length', len(file_data)),
+                                ],
+                            )
+            except Exception:
+                _logger.exception('error accessing %r', target_file)
+                return werkzeug.exceptions.InternalServerError('An error occured attempting to access %r; please let IT know.' % (str(target_file),))
         return werkzeug.exceptions.Forbidden(
-                '<p>You do not have permission to access %s.</p>'
-                '<p>Please see a supervisor to grant permission, or IT '
-                'if you do but are still getting this error.</p>'
+                'You do not have permission to access %s.\n\n'
+                'Please see a supervisor to grant permission, or IT '
+                'if you do but are still getting this error.'
                 % target_file)

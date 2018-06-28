69befd1c6569292163469b39ed96d5265d045b95
Ethan Furman <ethan@stoneleaf.us>
2018-06-28 15:35:12 -0700

    remove nonexistent file reference

--- __openerp__.py
+++ __openerp__.py
@@ -11,14 +11,15 @@
     'depends': [
             'base',
             'fnx',
-        ],
+            ],
     'js': [
-        ],
-    'css':['static/src/css/field_binary_css.css',],
+            ],
+    'css':[
+            ],
     'data': [
             'security/security.xml',
             'security/ir.model.access.csv',
-        ],
+            ],
     'test': [],
     'installable': True,
     'active': False,

b4bfe8486a284946e083adc64e2080b4e47f97cc
Ethan Furman <ethan@stoneleaf.us>
2018-07-15 22:21:52 -0700

    split xaml, js, and css code into separate files; add file deletion

--- controllers.py
+++ controllers.py
@@ -11,12 +11,28 @@ from xaml import Xaml
 
 _logger = logging.getLogger(__name__)
 fnx_root = Path('/var/openerp/fnxfs/')
+base = Path(__file__).path
+template = base/'/static/lib/file_upload.xaml'
+
 
 class FnxFS(Controller):
 
     _cp_path = Path('/fnxfs')
 
+    def _get_template(self):
+        current_timestamp = template.stat()[-3:]
+        if not self._template_timestamp or self._template_timestamp != current_timestamp:
+            with open(template) as t:
+                upload_template = t.read()
+            self._template = Xaml(upload_template).document.pages[0]
+        return self._template
+
+    def __init__(self, *args, **kwds):
+        super(FnxFS, self).__init__(*args, **kwds)
+        self._template_timestamp = None
+
     def __getattr__(self, name):
+        _logger.warning('calling __getattr__ for %r', name)
         return self.get_file
 
     # TODO: get_file needs to figure out who is asking, and check that s/he has permission
@@ -57,6 +73,29 @@ class FnxFS(Controller):
                 % target_file)
 
     @httprequest
+    def static(self, request, *args, **kwds):
+        target_file = Path(request.httprequest.path[7:])
+        target_path_file = base/target_file
+        if not target_path_file.exists() or target_path_file.isdir():
+            return request.not_found('%s is not available at this time.' % (target_file,))
+        try:
+            with (target_path_file).open('rb') as fh:
+                file_data = fh.read()
+            return request.make_response(
+                    file_data,
+                    headers=[
+                        ('Content-Disposition',  content_disposition(target_file.filename, request)),
+                        ('Content-Type', guess_type(target_file.filename)[0] or 'octet-stream'),
+                        ('Content-Length', len(file_data)),
+                        ],
+                    )
+        except Exception:
+            _logger.exception('error accessing %r', target_file)
+            return werkzeug.exceptions.InternalServerError(
+                    'An error occured attempting to access %r; please let IT know.'
+                    % (str(target_file),))
+
+    @httprequest
     def download(self, request, path, folder, file):
         target_path_file = fnx_root
         target_path_file /= path
@@ -80,12 +119,41 @@ class FnxFS(Controller):
                     )
 
     @httprequest
+    def delete(self, request, path, folder, file):
+        try:
+            master_session_id = request.httprequest.cookies['instance0|session_id'].replace('%22','')
+            master_session = request.httpsession[master_session_id]
+            login = master_session._login
+        except Exception:
+            _logger.exception('unauthorized attempt to delete %r', file)
+            return werkzeug.exceptions.Forbidden()
+        target_path_file = fnx_root
+        target_path_file /= path
+        target_path_file /= folder.replace('/', '%2f')
+        target_path_file /= file.replace('/', '%2f')
+        try:
+            _logger.info("user: %r; action: delete; file: '%s'", login, target_path_file)
+            target_path_file.unlink()
+            return request.make_response(
+                    '<i>file deleted</i>',
+                    headers=[
+                        ('Content-Type', 'text/html'),
+                        ('Content-Length', 19),
+                        ],
+                    )
+        except Exception:
+            _logger.exception('error deleting %r', file)
+            return werkzeug.exceptions.InternalServerError()
+
+    @httprequest
     def select_files(self, request, path, folder):
+        master_session_id = request.httprequest.cookies['instance0|session_id'].replace('%22','')
+        master_session_id
         target_path = fnx_root
         target_path /= path
         target_path /= folder.replace('/', '%2f')
         files = target_path.listdir()
-        template = Xaml(upload_template).document.pages[0]
+        template = self._get_template()
         page = template.string(files=files, folder=folder, path=path)
         try:
             return request.make_response(
@@ -97,9 +165,7 @@ class FnxFS(Controller):
                     )
         except Exception:
             _logger.exception('error processing %r', folder)
-            return werkzeug.exceptions.InternalServerError(
-                    'An error occured attempting to access %r; please let IT know.' % (str(folder),)
-                    )
+            return werkzeug.exceptions.InternalServerError()
 
     @httprequest
     def upload(self, request, path, folder, file):
@@ -107,140 +173,13 @@ class FnxFS(Controller):
         target_path_file /= path
         target_path_file /= folder.replace('/', '%2f')
         target_path_file /= file.filename.replace('/', '%2f')
-        _logger.info('receiving: %r', target_path_file)
+        try:
+            master_session_id = request.httprequest.cookies['instance0|session_id'].replace('%22','')
+            master_session = request.httpsession[master_session_id]
+            login = master_session._login
+        except Exception:
+            _logger.exception('unauthorized attempt to upload %r', file)
+            return werkzeug.exceptions.Forbidden()
+        _logger.info("user: %r; action: upload; file: '%s'", login, target_path_file)
         file.save(target_path_file)
         return "%s successfully uploaded" % file.filename
-
-
-upload_template = """\
-~h3:  =args.folder
-~form #file-catcher method='post'
-    ~input #file-input type='file' multiple style='width: 75px;'
-    ~input #folder-name type='hidden' name='folder' value=args.folder
-    ~input #path-name type='hidden' name='path' value=args.path
-    ~button type='submit'
-        Submit
-    ~button type='button' onclick="window.open('','_self','');window.close()": Close
-
-~div #add-file-list-display
-    ~h4:  Files to add
-    ~p #no-new-files: None selected.
-    ~ul #yes-new-files style='display: none;'
-~br
-~div #existing-file-list-display
-    ~h4:  Existing files
-    -if not args.files:
-        ~p #no-old-files: None
-        ~ul #yes-old-files style='display: none;'
-    -else:
-        ~p #no-old-files style='display: none;': None
-        ~ul #yes-old-files
-            -for f in args.files:
-                ~li: =f
-
-:javascript
-    (function () {
-        var rootdomain = window.location.origin
-        var fileCatcher = document.getElementById('file-catcher');
-        var fileInput = document.getElementById('file-input');
-        var addFileListDisplay = document.getElementById('add-file-list-display');
-        var noNewFiles = document.getElementById('no-new-files');
-        var yesNewFiles = document.getElementById('yes-new-files');
-        var noOldFiles = document.getElementById('no-old-files');
-        var yesOldFiles = document.getElementById('yes-old-files');
-        var existingFileListDisplay = document.getElementById('existing-file-list-display');
-        var existingFileNames = new Set();
-        var fileList = [];
-        var fileListNames = new Set();
-        var renderFileList, sendFile;
-        //
-        fileInput.value = '';
-        oldChildren = yesOldFiles.children;
-        for (var i = 0; i < oldChildren.length; i++) {
-            existingFileNames.add(oldChildren[i].innerHTML);
-            };
-        //
-        fileCatcher.addEventListener('submit', function (e) {
-            e.preventDefault();
-            var index = 0
-            fileList.forEach(function (file) {
-                sendFile(file, index);
-                index += 1
-                });
-            });
-        //
-        fileInput.addEventListener('change', function (e) {
-            for (var i = 0, proceed = false; i < fileInput.files.length; i++) {
-                var file = fileInput.files[i];
-                if (!fileListNames.has(file.name)) {
-                    fileListNames.add(file.name);
-                    fileList.push(file);
-                    proceed = true;
-                    } else {
-                    alert('File '+file.name+' already queued');
-                    };
-                };
-            if (proceed) {
-                renderFileList();
-                };
-            });
-        //
-        renderFileList = function () {
-            noNewFiles.style.display = 'none';
-            yesNewFiles.innerHTML = '';
-            fileList.forEach(function (file, index) {
-                yesNewFiles.style.display = '';
-                var fileDisplayEl = document.createElement('li');
-                fileDisplayEl.innerHTML = file.name;
-                fileDisplayEl.id = 'file-'+index;
-                var tracking = document.createElement('span');
-                tracking.id = 'file-span-'+index;
-                tracking.innerHTML = '&nbsp;&nbsp;-->&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Ready';
-                fileDisplayEl.appendChild(tracking);
-                yesNewFiles.appendChild(fileDisplayEl);
-                });
-            };
-        //
-        sendFile = function (file, index) {
-            var formData = new FormData();
-            var request = new XMLHttpRequest();
-            var fileSpanEl = document.getElementById('file-span-'+index);
-            //
-            request.onreadystatechange = function() {
-                if (request.readyState == 1) {
-                    fileSpanEl.innerHTML = '&nbsp;&nbsp;&nbsp;&nbsp;-->&nbsp;&nbsp;&nbsp;&nbsp;Sending...';
-                } else if (request.readyState == 4) {
-                    if (request.status != 200) {
-                        fileSpanEl.innerHTML = '&nbsp;&nbsp;&nbsp;&nbsp;-->&nbsp;&nbsp;'+request.statusText;
-                        fileSpanEl.style.color = 'red';
-                    } else {
-                        fileSpanEl.innerHTML = '&nbsp;&nbsp;&nbsp;&nbsp;-->&nbsp;&nbsp;&nbsp;&nbsp;Done';
-                        noOldFiles.style.display = 'none';
-                        yesOldFiles.style.display = '';
-                        trackingEl = document.getElementById('file-span-'+index);
-                        trackingEl.id = '';
-                        trackingEl.remove();
-                        fileEl = document.getElementById('file-'+index)
-                        fileEl.id = '';
-                        if (existingFileNames.has(fileEl.innerHTML)) {
-                            fileEl.remove();
-                        } else {
-                            yesOldFiles.appendChild(fileEl);
-                        };
-                        fileList.splice(fileList.indexOf(file), 1);
-                        fileListNames.delete(file.name);
-                        if (yesNewFiles.children.length == 0) {
-                            noNewFiles.style.display = '';
-                            yesNewFiles.style.display = 'none';
-                        };
-                    };
-                }
-            }
-            formData.append('file', file);
-            formData.append('folder', document.getElementById('folder-name').value);
-            formData.append('path', document.getElementById('path-name').value);
-            request.open("POST", rootdomain+"/fnxfs/upload");
-            request.send(formData);
-            };
-        })();
-"""
---
+++ static/lib/file_upload.js
@@ -0,0 +1,179 @@
+var file_upload = function () {
+    var rootdomain = window.location.origin
+    var formFileCatcher = document.getElementById('file_catcher');
+    var inputFile = document.getElementById('file_input');
+    var divAddFile = document.getElementById('add_file_div');
+    var pNoNewFiles = document.getElementById('no_new_files');
+    var ulYesNewFiles = document.getElementById('yes_new_files');
+    var pNoOldFiles = document.getElementById('no_old_files');
+    var ulYesOldFiles = document.getElementById('yes_old_files');
+    var divExistingFile = document.getElementById('existing_file_div');
+    // 
+    var existingFileNames = new Set();
+    var incomingFileNames = new Set();
+    var incomingFiles = [];
+    //
+    var oldChildren = ulYesOldFiles.children;
+    for (var i = 0; i < oldChildren.length; i++) {
+        existingFileNames.add(oldChildren[i].innerHTML);
+    };
+    //
+    inputFile.value = '';
+    //
+    formFileCatcher.addEventListener('submit', function (e) {
+        e.preventDefault();
+        incomingFiles.forEach(function (file) {
+            sendFile(file);
+        });
+    });
+    //
+    inputFile.addEventListener('change', function (e) {
+        for (var i = 0; i < inputFile.files.length; i++) {
+            var file = inputFile.files[i];
+            lowerName = file.name.toLowerCase()
+            if (incomingFileNames.has(lowerName)) {
+                alert('File '+file.name+' already queued');
+            } else if (existingFileNames.has(lowerName)) {
+                alert('File '+file.name+' already exists');
+            } else {
+                incomingFileNames.add(file.name);
+                incomingFiles.push(file);
+            };
+        };
+        renderFileList(incomingFiles, pNoNewFiles, ulYesNewFiles, true);
+    });
+    //
+    divAddFile.addEventListener('click', function (e) {
+        if (e.target.tagName=='BUTTON' && e.target.className=='icons') {
+            var garbage = e.target.parentElement
+            var fileName = garbage.children[1].innerHTML
+            console.log(fileName);
+            incomingFiles.forEach(function (file) {
+                if (file.name == fileName) {
+                    incomingFiles.splice(incomingFiles.indexOf(file), 1);
+                    incomingFileNames.delete(file.name);
+                    return;
+                };
+            });
+            console.log('garbage', garbage);
+            garbage.remove();
+        };
+    });
+    //
+    divExistingFile.addEventListener('click', function (e) {
+        if (e.target.tagName=='BUTTON' && e.target.className=='icons') {
+            console.log('deleting existing file!');
+            var garbage = e.target.parentElement
+            var fileName = garbage.children[1].innerHTML
+            console.log(fileName);
+            var request = new XMLHttpRequest();
+            request.onreadystatechange = function() {
+                console.log('readyState:', request.readyState);
+                if (request.readyState == 4) {
+                    if (request.status != 200) {
+                        spanError = document.createElement('span');
+                        spanError.innerHTML = '-->'+request.statusText;
+                        spanError.style.color = 'red';
+                        garbage.appendChild(spanError);
+                    } else {
+                        garbage.remove()
+                        existingFiles.delete(fileName);
+                        if (ulYesOldFiles.children.length == 0) {
+                            pNoOldFiles.style.display = '';
+                            ulYesOldFiles.style.display = 'none';
+                        };
+                    };
+                };
+            };
+        var formData = new FormData();
+        formData.append('file', fileName);
+        formData.append('folder', document.getElementById('folder_name').value);
+        formData.append('path', document.getElementById('path_name').value);
+        formData.append('token', document.getElementById('access_token').value);
+        request.open("POST", rootdomain+"/fnxfs/delete");
+        request.send(formData);
+        };
+    });
+    //
+    var renderFileList = function (files, para, list, includeStatus) {
+        console.log('files', files);
+        console.log('para', para);
+        console.log('list', list);
+        console.log('includeStatus', includeStatus);
+        if (!files.length) {
+            para.style.display = '';
+            list.style.display = 'none';
+        } else {
+            para.style.display = 'none';
+            list.innerHTML = '';
+            files.forEach(function (file, index) {
+                list.style.display = '';
+                var fileButton = document.createElement('button');
+                fileButton.innerHTML = 'd';
+                fileButton.className = 'icons';
+                var fileName = document.createElement('span');
+                fileName.innerHTML = file.name;
+                var fileDisplay = document.createElement('li');
+                fileDisplay.appendChild(fileButton);
+                fileDisplay.appendChild(fileName);
+                if (includeStatus) {
+                    var tracking = document.createElement('span');
+                    tracking.innerHTML = '--> Ready';
+                    fileDisplay.appendChild(tracking);
+                };
+                list.appendChild(fileDisplay);
+            });
+        };
+    };
+    //
+    var sendFile = function (file, index) {
+        var fileDisplay = (function () {
+            console.log('looking for:', file.name);
+            var children = ulYesNewFiles.children
+            for (var i = 0; i < children.length; i++) {
+                var liFile = children[i]
+                var fileName = liFile.children[1].innerHTML;
+                console.log('checking', fileName);
+                if (fileName == file.name) {
+                    console.log('found it!');
+                    return liFile;
+                };
+            };
+        })();
+        var spanTracking = fileDisplay.children[2];
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function() {
+            if (request.readyState == 1) {
+                spanTracking.innerHTML = '--> Sending...';
+            } else if (request.readyState == 4) {
+                if (request.status != 200) {
+                    spanTracking.innerHTML = '-->'+request.statusText;
+                    spanTracking.style.color = 'red';
+                } else {
+                    spanTracking.innerHTML = '--> Done';
+                    pNoOldFiles.style.display = 'none';
+                    ulYesOldFiles.style.display = '';
+                    spanTracking.remove()
+                    fileEl = fileDisplay.children[1]
+                    if (existingFileNames.has(fileEl.innerHTML)) {
+                        fileDisplay.remove();
+                    } else {
+                        ulYesOldFiles.appendChild(fileDisplay);
+                    };
+                    incomingFiles.splice(incomingFiles.indexOf(file), 1);
+                    incomingFileNames.delete(file.name);
+                    if (ulYesNewFiles.children.length == 0) {
+                        pNoNewFiles.style.display = '';
+                        ulYesNewFiles.style.display = 'none';
+                    };
+                };
+            };
+        };
+        var formData = new FormData();
+        formData.append('file', file);
+        formData.append('folder', document.getElementById('folder_name').value);
+        formData.append('path', document.getElementById('path_name').value);
+        request.open("POST", rootdomain+"/fnxfs/upload");
+        request.send(formData);
+    };
+};

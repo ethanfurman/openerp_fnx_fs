ed126d982da07fd5cf79edfa80eb3a24c05a706f
Ethan Furman <ethan@stoneleaf.us>
2019-05-04 14:22:23 -0700

    return False when no files found

--- fields.py
+++ fields.py
@@ -74,6 +74,8 @@ class files(fields.function):
             display_files = []
             if base_path.exists(disk_folder):
                 display_files = sorted((base_path/disk_folder).listdir())
+            if not display_files:
+                continue
             safe_files = [quote(f, safe='') for f in display_files]
             res[id] = template.string(
                     download=website_download,
